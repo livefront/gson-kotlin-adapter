542018ce17049ef9adadf375cf55d5f85244bf8d
David Perez <david@livefront.com>
2023-06-02 14:28:13 -0500

    Update testing and code-coverage dependencies.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,14 +6,14 @@ object Versions {
     object JetBrains {
         // This needs to be kept in sync with the build.gradle.kts file for 'buildSrc'.
         const val kotlin = "1.8.21"
-        const val kover = "0.6.1"
+        const val kover = "0.7.1"
     }
 
     object Junit {
-        const val jupiter = "5.9.1"
+        const val jupiter = "5.9.3"
     }
 
     object Mockk {
-        const val mockk = "1.13.3"
+        const val mockk = "1.13.5"
     }
 }
